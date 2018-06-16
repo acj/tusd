1aa9ec508013a8a1ae450fd949e73c7c78b0dd77
Adam Jensen <acjensen@gmail.com>
2018-06-16 15:03:59 -0400

    Use 50MB chunks for writing to S3

--- s3store/s3store.go
+++ s3store/s3store.go
@@ -571,47 +571,48 @@ func isAwsError(err error, code string) bool {
 }
 
 func (store S3Store) calcOptimalPartSize(size int64) (optimalPartSize int64, err error) {
-	switch {
-	// When upload is smaller or equal MinPartSize, we upload in just one part.
-	case size <= store.MinPartSize:
-		optimalPartSize = store.MinPartSize
-	// Does the upload fit in MaxMultipartParts parts or less with MinPartSize.
-	case size <= store.MinPartSize*store.MaxMultipartParts:
-		optimalPartSize = store.MinPartSize
-	// Prerequisite: Be aware, that the result of an integer division (x/y) is
-	// ALWAYS rounded DOWN, as there are no digits behind the comma.
-	// In order to find out, whether we have an exact result or a rounded down
-	// one, we can check, whether the remainder of that division is 0 (x%y == 0).
-	//
-	// So if the result of (size/MaxMultipartParts) is not a rounded down value,
-	// then we can use it as our optimalPartSize. But if this division produces a
-	// remainder, we have to round up the result by adding +1. Otherwise our
-	// upload would not fit into MaxMultipartParts number of parts with that
-	// size. We would need an additional part in order to upload everything.
-	// While in almost all cases, we could skip the check for the remainder and
-	// just add +1 to every result, but there is one case, where doing that would
-	// doom our upload. When (MaxObjectSize == MaxPartSize * MaxMultipartParts),
-	// by adding +1, we would end up with an optimalPartSize > MaxPartSize.
-	// With the current S3 API specifications, we will not run into this problem,
-	// but these specs are subject to change, and there are other stores as well,
-	// which are implementing the S3 API (e.g. RIAK, Ceph RadosGW), but might
-	// have different settings.
-	case size%store.MaxMultipartParts == 0:
-		optimalPartSize = size / store.MaxMultipartParts
-	// Having a remainder larger than 0 means, the float result would have
-	// digits after the comma (e.g. be something like 10.9). As a result, we can
-	// only squeeze our upload into MaxMultipartParts parts, if we rounded UP
-	// this division's result. That is what is happending here. We round up by
-	// adding +1, if the prior test for (remainder == 0) did not succeed.
-	default:
-		optimalPartSize = size/store.MaxMultipartParts + 1
-	}
+	// switch {
+	// // When upload is smaller or equal MinPartSize, we upload in just one part.
+	// case size <= store.MinPartSize:
+	// 	optimalPartSize = store.MinPartSize
+	// // Does the upload fit in MaxMultipartParts parts or less with MinPartSize.
+	// case size <= store.MinPartSize*store.MaxMultipartParts:
+	// 	optimalPartSize = store.MinPartSize
+	// // Prerequisite: Be aware, that the result of an integer division (x/y) is
+	// // ALWAYS rounded DOWN, as there are no digits behind the comma.
+	// // In order to find out, whether we have an exact result or a rounded down
+	// // one, we can check, whether the remainder of that division is 0 (x%y == 0).
+	// //
+	// // So if the result of (size/MaxMultipartParts) is not a rounded down value,
+	// // then we can use it as our optimalPartSize. But if this division produces a
+	// // remainder, we have to round up the result by adding +1. Otherwise our
+	// // upload would not fit into MaxMultipartParts number of parts with that
+	// // size. We would need an additional part in order to upload everything.
+	// // While in almost all cases, we could skip the check for the remainder and
+	// // just add +1 to every result, but there is one case, where doing that would
+	// // doom our upload. When (MaxObjectSize == MaxPartSize * MaxMultipartParts),
+	// // by adding +1, we would end up with an optimalPartSize > MaxPartSize.
+	// // With the current S3 API specifications, we will not run into this problem,
+	// // but these specs are subject to change, and there are other stores as well,
+	// // which are implementing the S3 API (e.g. RIAK, Ceph RadosGW), but might
+	// // have different settings.
+	// case size%store.MaxMultipartParts == 0:
+	// 	optimalPartSize = size / store.MaxMultipartParts
+	// // Having a remainder larger than 0 means, the float result would have
+	// // digits after the comma (e.g. be something like 10.9). As a result, we can
+	// // only squeeze our upload into MaxMultipartParts parts, if we rounded UP
+	// // this division's result. That is what is happending here. We round up by
+	// // adding +1, if the prior test for (remainder == 0) did not succeed.
+	// default:
+	// 	optimalPartSize = size/store.MaxMultipartParts + 1
+	// }
 
-	// optimalPartSize must never exceed MaxPartSize
-	if optimalPartSize > store.MaxPartSize {
-		return optimalPartSize, fmt.Errorf("calcOptimalPartSize: to upload %v bytes optimalPartSize %v must exceed MaxPartSize %v", size, optimalPartSize, store.MaxPartSize)
-	}
-	return optimalPartSize, nil
+	// // optimalPartSize must never exceed MaxPartSize
+	// if optimalPartSize > store.MaxPartSize {
+	// 	return optimalPartSize, fmt.Errorf("calcOptimalPartSize: to upload %v bytes optimalPartSize %v must exceed MaxPartSize %v", size, optimalPartSize, store.MaxPartSize)
+	// }
+	//return optimalPartSize, nil
+	return 1024 * 1024 * 50, nil
 }
 
 func (store S3Store) pathInBucket(filename string) *string {
